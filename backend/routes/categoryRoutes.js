e9025a1a69dd732337143818ace99baa92d322cb
Riturajsharma707 <riturajsharma54321@gmail.com>
2024-05-27 23:34:26 +0530

    Category Backend done

--- backend/routes/categoryRoutes.js
+++ backend/routes/categoryRoutes.js
@@ -1,10 +1,24 @@
 import express from "express";
-import { createCategory } from "../controllers/categoryController.js";
+import {
+  createCategory,
+  updateCategory,
+  removeCategory,
+  listCategories,
+  readCategory,
+} from "../controllers/categoryController.js";
 
 const router = express.Router();
 
 import { authenticate, authorizeAdmin } from "../middlewares/authMiddleware.js";
 
-router.route("/").post(createCategory);
+router.route("/").post(authenticate, authorizeAdmin, createCategory);
+router.route("/:categoryId").put(authenticate, authorizeAdmin, updateCategory);
+router
+  .route("/:categoryId")
+  .delete(authenticate, authorizeAdmin, removeCategory);
+
+router.route("/categories").get(listCategories);
+
+router.route("/:id").get(readCategory);
 
 export default router;
