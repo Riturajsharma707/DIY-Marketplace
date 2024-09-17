8b66343f2af5ea3ae4a569d12ef30230316a9088
Riturajsharma707 <riturajsharma54321@gmail.com>
2024-09-17 17:57:16 +0530

    product backend section done

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -8,14 +8,37 @@ const router = express.Router();
 import {
   addProduct,
   updateProductDetails,
+  removeProduct,
+  fetchProducts,
+  fetchProductById,
+  fetchAllProducts,
+  addProductReview,
+  fetchTopProducts,
+  newproducts,
 } from "../controllers/productController.js";
 
 import { authenticate, authorizeAdmin } from "../middlewares/authMiddleware.js";
 import checkId from "../middlewares/checkId.js";
 
-router.route("/").post(authenticate, authorizeAdmin, formidable(), addProduct);
+router
+  .route("/")
+  .get(fetchProducts)
+  .post(authenticate, authorizeAdmin, formidable(), addProduct);
+
+router.route("/allproducts").get(fetchAllProducts);
+
+router
+  .route("/:id/reviews")
+  .post(authenticate, authorizeAdmin, checkId, addProductReview);
+
+router.get("/top", fetchTopProducts);
+router.get("/new", newproducts);
+
 router
   .route("/:id")
-  .post(authenticate, authorizeAdmin, formidable(), updateProductDetails);
+  .get(fetchProductById)
+  .put(authenticate, authorizeAdmin, formidable(), updateProductDetails);
+
+router.route("/:id").delete(authenticate, authorizeAdmin, removeProduct);
 
 export default router;
